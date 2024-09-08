b72f8487584edcfa1741fbd069848327d104879c
Matthew Jackson <matthew@hmjackson.com>
2024-09-08 10:54:44 +0100

    Fix GT sequence logging for LAM and tokenizer

--- train_lam.py
+++ train_lam.py
@@ -147,11 +147,11 @@ while step < args.num_steps:
     for videos in dataloader:
         # --- Train step ---
         rng, _rng = jax.random.split(rng)
-        inp = dict(
+        inputs = dict(
             videos=jnp.array(videos, dtype=jnp.float32) / 255.0,
             rng=_rng,
         )
-        train_state, loss, recon, metrics = train_step(train_state, inp)
+        train_state, loss, recon, metrics = train_step(train_state, inputs)
         print(f"Step {step}, loss: {loss}")
         step += 1
 
